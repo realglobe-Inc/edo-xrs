ee3a6a53a3da0fd899d9b827c8c69e9e209d6deb
jm <jm@example.com>
2015-08-06 13:58:47 +0900

    fix path related to so15 spec

--- main.go
+++ main.go
@@ -77,16 +77,16 @@ func main() {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "X-Experience-API-Version")
 	})
-	router.Get("/:user/:app/about", func(params martini.Params, w http.ResponseWriter) (int, string) {
+	router.Get("/v1/users/:user/services/:app/about", func(params martini.Params, w http.ResponseWriter) (int, string) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "X-Experience-API-Version")
 
 		return http.StatusOK, `{"version": ["1.0.0", "1.0.1", "1.0.2"]}`
 	})
-	router.Put("/:user/:app/statements", c.StoreStatement)
-	router.Post("/:user/:app/statements", c.StoreMultStatement)
-	router.Get("/:user/:app/statements", acceptlang.Languages(), c.FindStatement)
+	router.Put("/v1/users/:user/services/:app/statements", c.StoreStatement)
+	router.Post("/v1/users/:user/services/:app/statements", c.StoreMultStatement)
+	router.Get("/v1/users/:user/services/:app/statements", acceptlang.Languages(), c.FindStatement)
 
 	router.Run()
 }
